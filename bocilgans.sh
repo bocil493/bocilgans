9a0db62acac051f3d8abb6ed6e70c645b837f5b5
Bocilgans <57514844+bocil493@users.noreply.github.com>
2019-11-08 18:06:39 +0700

    Add files via upload

---
+++ bocilgans.sh
@@ -0,0 +1,98 @@
+#HALO LOSER
+#MAU NGAPAIN
+#JANGAN RECODE YA ASU
+#KECUALI UNTUK PEMBELAJARAN
+echo
+echo
+clear
+apt update && apt upgrade
+apt install nano
+apt install git
+pkg install python python2 vim figlet curl
+clear
+apt install php
+pip2 install mechanize
+pip2 install lolcat
+pip2 install requests
+clear
+
+
+
+
+
+
+echo "=====================================" | lolcat
+echo "⭐Author: B0C1L Gans" | lolcat
+echo "⭐Team : Reaction Of System" | lolcat
+echo "⭐WhatsApp : +62 856-9215-8837" | lolcat
+echo "=====================================" | lolcat
+echo ""
+echo ""
+echo "WELCOME TO SCRIPT PEMROGRAMAN BASH!" | lolcat
+echo ""
+echo ""
+echo "—————————————————————————————————————" | lolcat
+echo ">>>1.REPORT FB" [√] | lolcat
+echo ">>>2 SPAMWHATSAPP" [√] | lolcat
+echo ">>>3.LACAK LOKASI" [√] | lolcat
+echo ">>>4.SPAM SMS BRUTAL TERBARU" [√] | lolcat
+echo ">>>00.Exit" | lolcat
+echo "—————————————————————————————————————" | lolcat
+read -p "Masukkan No Yang Anda Pilih : " pil
+if [ $pil = "1" ]
+then
+clear
+figlet -f slant "B 0 C 1 L" | lolcat
+sleep 1
+git clone https://github.com/IlayTamvan/Report.git
+clear
+cd Report
+unzip Report.zip
+clear
+python2 Report.py
+fi
+
+if [ $pil = "2" ]
+then
+clear
+figlet -f slant "B 0 C 1 L" | lolcat
+sleep 1
+git clone https://github.com/4L13199/LITESPAM.git
+clear
+cd LITESPAM
+sh LITESPAM.sh
+fi
+
+if [ $pil = "3" ]
+then
+clear
+figlet -f slant "B 0 C 1 L" | lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/locator.git
+clear
+cd locator
+bash locator.sh
+fi
+
+if [ $pil = "4" ]
+then
+clear
+figlet -f slant "B 0 C 1 L" | lolcat
+sleep 1
+git clone https://github.com/TERMUXID3/brutal-sms
+cd brutal-sms
+python2 run.py
+fi
+
+
+if [ $pil = "00" ]
+then
+clear
+figlet -f slant "B 0 C 1 L" | lolcat
+sleep 2
+echo "Terima Kasih Telah Menggunakan Tools Ini" | lolcat
+sleep 2
+echo "Semoga Bermanfaat Untuk Kalian" | lolcat
+exit
+fi
+
